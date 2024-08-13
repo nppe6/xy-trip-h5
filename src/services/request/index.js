b4a7a5459a6b071fd528dbe6bddf9031f4d4381c
nppe6 <xiao2313667401@foxmail.com>
2024-08-13 23:46:47 +0800

    二次封装axios

---
+++ src/services/request/index.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { BASE_URL, TIMEOUT } from './config'
+
+class XYRequest {
+  constructor(baseURL, timeout = 10000) {
+    this.instance = axios.create({
+      baseURL,
+      timeout
+    })
+  }
+
+  request(config) {
+    return new Promise((resolve, reject) => {
+      this.instance
+        .request(config)
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+  }
+
+  get(config) {
+    return this.request({ ...config, method: 'GET' })
+  }
+
+  post(config) {
+    return this.request({ ...config, method: 'POST' })
+  }
+}
+
+export default new XYRequest(BASE_URL, TIMEOUT)
